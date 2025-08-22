7de2632378ee465f41687ded0819db83fe8b0ee5
Srinath <srinathanilkumar@gmail.com>
2025-08-22 19:21:44 +0530

    add contact list data

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,23 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Dipesh",
+      email: "malvia@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Nikesh",
+      email: "nick@gmail.com",
+    },
+  ];
   return (
     <div className="ui container">
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
