7ca033a0caa78120c96201c538064bea6bad30a7
Srinath <srinathanilkumar@gmail.com>
2025-08-22 21:44:30 +0530

    add contacts logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Dipesh",
-      email: "malvia@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Nikesh",
-      email: "nick@gmail.com",
-    },
-  ];
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts((data) => [...data, contact]);
+    localStorage.setItem(
+      LOCAL_STORAGE_KEY,
+      JSON.stringify([...contacts, contact])
+    );
+  };
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    console.log("get", retrieveContacts);
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  // useEffect(() => {
+  //   console.log("set", contacts);
+
+  // }, [contacts]);
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts={contacts} />
     </div>
   );
